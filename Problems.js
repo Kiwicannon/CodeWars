12af24e3de91a06bc5c6204678f75f9aa9e9b922
Kiwicannon <stevencannon01@gmail.com>
2017-05-02 12:53:35 -0600

    stringToNumber

--- Problems.js
+++ Problems.js
@@ -828,6 +828,9 @@ function makeNegative(num) {
 // Question
 // ----------------------------------------------------------------------------------
 
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
 
 
 // Answer
@@ -835,10 +838,17 @@ function makeNegative(num) {
 // Mine
 // -----
 
+var stringToNumber = function(str){
+  // put your code here
+  return str/1;
+}
 
 // Best Practice
 // --------------
 
+var stringToNumber = function(str){
+  return parseInt(str);
+}
 
 // ----------------------------------------------------------------------------------
 // Question
