0551348de8f1bfa2be3fa1fb2d645a992e3b8328
Kiwicannon <stevencannon01@gmail.com>
2017-05-02 12:47:27 -0600

    countSheep

--- Problems.js
+++ Problems.js
@@ -773,17 +773,29 @@ function numberToString(num) {
 // Question
 // ----------------------------------------------------------------------------------
 
-
+// Consider an array of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
 // Answer
 // ----------------------------------------------------------------------------------
 // Mine
 // -----
 
+function countSheeps(arrayOfSheep) {
+  let count = 0
+  for(let i = 0; i < arrayOfSheep.length; i++){
+   if(arrayOfSheep[i] === true){
+   count++
+   }
+  }
+  return count
+}
 
 // Best Practice
 // --------------
 
+function countSheeps(arrayOfSheeps) {
+  return arrayOfSheeps.filter(Boolean).length;
+}
 
 // ----------------------------------------------------------------------------------
 // Question
