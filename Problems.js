330dd99960ba155e2815bd6872ecff27ae916ff8
Kiwicannon <stevencannon01@gmail.com>
2017-05-01 20:35:54 -0600

    descendingOrder

--- Problems.js
+++ Problems.js
@@ -593,17 +593,33 @@ function getMiddle(s)
 // Question
 // ----------------------------------------------------------------------------------
 
+// Your task is to make a function that can take any non-negative integer as a argument and return it with it's digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 
+// Examples:
+
+// Input: 21445 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 1254859723 Output: 9875543221
 
 // Answer
 // ----------------------------------------------------------------------------------
 // Mine
 // -----
 
+function descendingOrder(n){
+let a = n.toString().split('')
+   let b = a.sort(function(a, b){return b-a}).join('')
+  return (b/1) ;
+}
 
 // Best Practice
 // --------------
 
+function descendingOrder(n){
+  return parseInt(String(n).split('').sort().reverse().join(''))
+}
 
 // ----------------------------------------------------------------------------------
 // Question
