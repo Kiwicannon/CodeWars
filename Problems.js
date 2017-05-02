980e7d2c9c30d154962c167f1341e3a24b21bc46
Kiwicannon <stevencannon01@gmail.com>
2017-05-01 21:12:13 -0600

    arrayDiff

--- Problems.js
+++ Problems.js
@@ -667,17 +667,36 @@ function squareDigits(num){
 // Question
 // ----------------------------------------------------------------------------------
 
+// Your goal in this kata is to implement an difference function, which subtracts one list from another.
 
+// It should remove all values from list a, which are present in list b.
+
+// difference([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// difference([1,2,2,2,3],[2]) == [1,3]
 
 // Answer
 // ----------------------------------------------------------------------------------
 // Mine
 // -----
 
+function array_diff(a, b) {
+var diff = []
+for(var i = 0; i < a.length; i++){
+if(a[i] != b){
+diff.push(a[i])
+}
+}
+return diff
+}
 
 // Best Practice
 // --------------
 
+function array_diff(a, b) {
+  return a.filter(function(x) { return b.indexOf(x) == -1; });
+}
 
 // ----------------------------------------------------------------------------------
 // Question
