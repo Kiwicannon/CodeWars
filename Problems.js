61455411eac609c578ecfc8d05af2f90bc5ed839
Kiwicannon <stevencannon01@gmail.com>
2017-05-09 21:52:28 -0600

    anagrams

--- Problems.js
+++ Problems.js
@@ -950,16 +950,45 @@ function solution(string) {
 // ----------------------------------------------------------------------------------
 
 
+// What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+// 'abba' & 'baab' == true
+
+// 'abba' & 'bbaa' == true
+
+// 'abba' & 'abbba' == false
+// Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+// anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+// anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+// anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+
 
 // Answer
 // ----------------------------------------------------------------------------------
 // Mine
 // -----
 
+function anagrams(word, words) {
+  return words.filter(function(w){
+    return w.split('').sort().join('') === word.split('').sort().join('');
+  });
+}
 
 // Best Practice
 // --------------
 
+String.prototype.sort = function() {
+  return this.split("").sort().join("");
+};
+
+function anagrams(word, words) {
+  return words.filter(function(x) {
+      return x.sort() === word.sort();
+  });
+}
 
 
 // ----------------------------------------------------------------------------------
