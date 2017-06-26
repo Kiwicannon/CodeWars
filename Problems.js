08351bfac80831d3e1b555f4444743783c0e4963
Kiwicannon <stevencannon01@gmail.com>
2017-06-26 08:51:58 -0600

    are you playing banjo

--- Problems.js
+++ Problems.js
@@ -1160,18 +1160,46 @@ function sumArray(array) {
 // Question
 // ----------------------------------------------------------------------------------
 
+// Create a function which answers the question "Are you playing Banjo?". If your name starts with the letter "R" or lower case "r", you are playing Banjo!
 
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + "plays banjo" 
+// name + "does not play banjo"
+// Names given are always valid strings.
 
 // Answer
 // ----------------------------------------------------------------------------------
 // Mine
 // -----
 
+function areYouPlayingBanjo(name) {
+  if(name[0] === 'r' || name[0] === 'R'){
+  
+  return name + ' plays banjo' ;}
+  else {
+  return name + ' does not play banjo'
+  }
+}
 
 // Best Practice
 // --------------
 
+function areYouPlayingBanjo(name) {
+  return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+}
 
+function areYouPlayingBanjo(name) {
+  if (name[0].toLowerCase() === 'r') {
+    return name + ' plays banjo';
+  } else {
+    return name + ' does not play banjo';
+  }
+}
+
+function areYouPlayingBanjo(name) {
+  return name + (/^r/i.test(name) ? " plays " : " does not play ") + "banjo";
+}
 
 // ----------------------------------------------------------------------------------
 // Question
