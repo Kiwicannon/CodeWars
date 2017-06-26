0247190a050904476e5cece84b37c4fad921fbe7
Kiwicannon <stevencannon01@gmail.com>
2017-06-26 08:57:23 -0600

    freeze

--- Problems.js
+++ Problems.js
@@ -1205,6 +1205,7 @@ function areYouPlayingBanjo(name) {
 // Question
 // ----------------------------------------------------------------------------------
 
+// There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
 
 
 // Answer
@@ -1212,11 +1213,32 @@ function areYouPlayingBanjo(name) {
 // Mine
 // -----
 
+Object.freeze(MrFreeze)
 
 // Best Practice
 // --------------
 
+function deepFreeze (o) {
+  var prop, propKey;
+  
+  Object.freeze( o );
+  for ( propKey in o ) {
+    prop = o[ propKey ];
+    
+    if ( !o.hasOwnProperty( propKey ) || !(typeof prop === "object") || Object.isFrozen( prop ) ) {
+      continue;
+    }
 
+    deepFreeze(prop);
+  }
+}
+
+deepFreeze(MrFreeze);
+
+// mark the MrFreeze object instance as frozen
+
+var MyFreeze = {};
+(Object.freeze || object)(MrFreeze);
 
 // ----------------------------------------------------------------------------------
 // Question
