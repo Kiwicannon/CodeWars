8240151916b80b0258552ffd5038b8b66eabf78b
Kiwicannon <stevencannon01@gmail.com>
2017-06-27 12:24:13 -0600

    smash

--- Problems.js
+++ Problems.js
@@ -1677,6 +1677,14 @@ function litres(time) {
 // Question
 // ----------------------------------------------------------------------------------
 
+// Sentence Smash
+
+// Write a method smash that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+
+// var words = ['hello', 'world', 'this', 'is', 'great'];
+// smash(words); // returns "hello world this is great"
 
 
 // Answer
@@ -1684,10 +1692,16 @@ function litres(time) {
 // Mine
 // -----
 
+smash = (words) => {return words.join(', ').replace(/,/g , "")};
 
 // Other
 // --------------
 
+smash = function (words) {
+  return words.join(" ");
+};
+
+let smash = words => words.join(" ");
 
 
 // ----------------------------------------------------------------------------------
