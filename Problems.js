9da8e33b14e1f60f15935aca3e2ade3db1ace206
Kiwicannon <stevencannon01@gmail.com>
2017-06-27 11:54:50 -0600

    add

--- Problems.js
+++ Problems.js
@@ -1503,6 +1503,15 @@ websites.push("codewars")
 // Question
 // ----------------------------------------------------------------------------------
 
+// Sum Array
+
+// Write a method sum (sum_array in python) that takes an array of numbers and returns the sum of the numbers. These may be integers or decimals for Ruby and any instance of Num for Haskell. The numbers can also be negative. If the array does not contain any numbers then you should return 0.
+
+// Examples
+
+// numbers = [1, 5.2, 4, 0, -1]
+// puts sum(numbers)
+// 9.2
 
 
 // Answer
@@ -1510,11 +1519,37 @@ websites.push("codewars")
 // Mine
 // -----
 
+// Sum Numbers
+function sum (numbers) {
+    "use strict";
+   return numbers[0] == null ? 0 : (numbers.reduce( function (a, b){
+   return a + b }))
+    
+};
 
 // Best Practice
 // --------------
 
+sum = function (numbers) {
+  "use strict";
+  return numbers.reduce(function(t, n){
+    return t + n;
+  }, 0);
+};
 
+function sum(numbers) {
+  return numbers.reduce((a, b) => a + b, 0);
+}
+
+
+sum = function (numbers) {
+  "use strict";
+  var total = 0;
+  for(var i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+  return total;
+};
 
 // ----------------------------------------------------------------------------------
 // Question
