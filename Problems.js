af28e141987a477da578f87912db3f206eba24e8
Kiwicannon <stevencannon01@gmail.com>
2017-07-14 12:43:31 -0600

    stringy

--- Problems.js
+++ Problems.js
@@ -1844,17 +1844,45 @@ function _if(bool, func1, func2) {
 // Question
 // ----------------------------------------------------------------------------------
 
+// write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
 
+// the string should start with a 1.
+
+// a string with size 6 should return :'101010'.
+
+// with size 4 should return : '1010'.
+
+// with size 12 should return : '101010101010'.
+
+// The size will always be positive and will only use whole numbers.
 
 // Answer
 // ----------------------------------------------------------------------------------
 // Mine
 // -----
 
+function stringy(size) {
+var str = '' 
+  for(let i = 0; i < size; i++){
+  
+   i % 2 == 0 ? str += '1' : str += '0'
+   }
+   return str
+}
 
 // Other
 // --------------
 
+function stringy(size) {
+  var str='';
+  for( var i=1; i<=size; i++ )
+    str+=i%2;
+  return str;
+}
+
+const stringy = size => Array.from(Array(size), (v, i) => +!(i % 2)).join('');
+
+const stringy = size => "10".repeat(size).slice(0,size);
 
 
 // ----------------------------------------------------------------------------------
